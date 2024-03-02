d10fdfa2e1be4a9b3a8812b39e8f025c10c3290a
SBK850 <160433022+SBK850@users.noreply.github.com>
2024-03-02 22:17:27 +0000

    Update twitter.js

--- twitter.js
+++ twitter.js
@@ -3,11 +3,7 @@ const axios = require('axios');
 const cors = require('cors');
 const app = express();
 
-
-app.use(cors({
-  origin: '*'
-}));
-
+app.use(cors());
 app.use(express.json());
 
 app.post('/get-twitter-embed', async (req, res) => {
@@ -17,11 +13,34 @@ app.post('/get-twitter-embed', async (req, res) => {
         const twitterOEmbedUrl = `https://publish.twitter.com/oembed?url=${encodedUrl}&widget=Tweet`;
         const response = await axios.get(twitterOEmbedUrl);
 
-        res.setHeader('Content-Type', 'text/html');
-        res.send(response.data.html);
+        res.json({ html: response.data.html });
     } catch (error) {
-        console.error('Error fetching Twitter embed code:', error);
-        res.status(500).json({ error: 'Failed to fetch Twitter embed code' });
+        console.error('Error fetching Twitter embed code:', error.message);
+
+        if (error.response) {
+
+            console.error('Response data:', error.response.data);
+            console.error('Response status:', error.response.status);
+            console.error('Response headers:', error.response.headers);
+            res.status(500).json({
+                error: 'Failed to fetch Twitter embed code',
+                details: error.response.data,
+                status: error.response.status,
+            });
+        } else if (error.request) {
+
+          console.error('Request error:', error.request);
+            res.status(500).json({
+                error: 'No response received when fetching Twitter embed code',
+            });
+        } else {
+
+          console.error('Error message:', error.message);
+            res.status(500).json({
+                error: 'Error setting up request to Twitter API',
+                message: error.message,
+            });
+        }
     }
 });
 
