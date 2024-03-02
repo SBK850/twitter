129d5f767973e8ba7328f6c0e9b206a513fef3a4
SBK850 <160433022+SBK850@users.noreply.github.com>
2024-03-02 22:04:49 +0000

    Update twitter.js

--- twitter.js
+++ twitter.js
@@ -3,7 +3,11 @@ const axios = require('axios');
 const cors = require('cors');
 const app = express();
 
-app.use(cors()); 
+
+app.use(cors({
+  origin: '*'
+}));
+
 app.use(express.json());
 
 app.post('/get-twitter-embed', async (req, res) => {
@@ -13,7 +17,8 @@ app.post('/get-twitter-embed', async (req, res) => {
         const twitterOEmbedUrl = `https://publish.twitter.com/oembed?url=${encodedUrl}&widget=Tweet`;
         const response = await axios.get(twitterOEmbedUrl);
 
-        res.json({ html: response.data.html });
+        res.setHeader('Content-Type', 'text/html');
+        res.send(response.data.html);
     } catch (error) {
         console.error('Error fetching Twitter embed code:', error);
         res.status(500).json({ error: 'Failed to fetch Twitter embed code' });
